5a7c04e67421877132fe80ae547a87d6885d68f2
nchchow <nelsonchow91.nc@gmail.com>
2020-04-29 18:51:58 -0700

    optimized speed in removing comments on page by removing last child

--- sprint-two/scripts/app.js
+++ sprint-two/scripts/app.js
@@ -1,24 +1,3 @@
-const comments = [
-	{
-		userName: "Theodore Duncan",
-		datePosted: "11/15/2018",
-		content:
-			"How can someone be so good!!! You can tell he lives for this and loves to do it every day. Everytime I see him I feel instantly happy! He’s definitely my favorite ever!",
-	},
-	{
-		userName: "Gary Wong",
-		datePosted: "12/12/2018",
-		content:
-			"Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
-	},
-	{
-		userName: "Micheal Lyons",
-		datePosted: "12/18/2018",
-		content:
-			"They BLEW the ROOF off at their last show, once everyone started figuring out they were going. This is still simply the greatest opening of a concert I have EVER witnessed.",
-	},
-];
-
 /*
 	TAKES COMMENTS, BUILDS HTML ELEMENTS AND RENDERS ON SCREEN
 */
@@ -33,7 +12,8 @@ newCommentForm.addEventListener("submit", (e) => {
 	// construct a new comment and push to comments array
 	comments.push(newComment(e.target));
 	// clear all comments on screen
-	commentsList.innerHTML = "";
+	// commentsList.innerHTML = "";
+	removePageComments();
 	// render comments
 	render(comments);
 	// clear input fields
@@ -41,7 +21,9 @@ newCommentForm.addEventListener("submit", (e) => {
 });
 
 // render comments on load
-render(comments);
+(function init() {
+	render(comments);
+})();
 
 // renders comments to screen in reverse chrono order
 function render(comments) {
@@ -52,6 +34,9 @@ function render(comments) {
 	}
 }
 
+/* 
+COMMENT CONSTRUCTOR FUNCTIONS 
+*/
 // helper function to create element and add class, returns the element
 function classElement(classStr, elementStr) {
 	const elem = document.createElement(elementStr);
@@ -59,7 +44,6 @@ function classElement(classStr, elementStr) {
 	return elem;
 }
 
-/* COMMENT CONSTRUCTOR FUNCTIONS */
 // contructs comment elem and returns it
 function displayComment(comment) {
 	const userIcon = classElement("comment__user-icon", "div");
@@ -109,29 +93,25 @@ function newComment(target) {
 }
 
 // // removes all comments and dividers on page
-// function removePageComments() {
-// 	commentsList.innerHTML = "";
-// 	// let elems = commentsList.children;
-// 	// console.log(elems);
-// 	// for (e of elems) {
-// 	// 	console.log(e);
-// 	// 	commentsList.removeChild(e);
-// 	// }
+function removePageComments() {
+	while (commentsList.lastChild) {
+		commentsList.removeChild(commentsList.lastChild);
+	}
 
-// 	// const commentElems = document.querySelectorAll(".comment");
-// 	// const dividers = document.querySelectorAll(".divider");
-// 	// commentElems.forEach((comment) => {
-// 	// 	commentsList.removeChild(comment);
-// 	// });
-// 	// // remove all except first divider
-// 	// for (let i = 1; i < dividers.length; i++) {
-// 	// 	commentsList.removeChild(dividers[i]);
-// 	// }
-// }
+	// let elems = commentsList.children;
+	// console.log(elems);
+	// for (e of elems) {
+	// 	console.log(e);
+	// 	commentsList.removeChild(e);
+	// }
 
-// // clear input fields
-// function clearInputs(target) {
-// 	// target.name.value = "";
-// 	// target.content.value = "";
-// 	target.reset();
-// }
+	// const commentElems = document.querySelectorAll(".comment");
+	// const dividers = document.querySelectorAll(".divider");
+	// commentElems.forEach((comment) => {
+	// 	commentsList.removeChild(comment);
+	// });
+	// // remove all except first divider
+	// for (let i = 1; i < dividers.length; i++) {
+	// 	commentsList.removeChild(dividers[i]);
+	// }
+}
